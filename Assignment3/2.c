2ad892151d248fcec7fdf3712f0b5c8be998b8a5
phukaoz <tnpkofficial@gmail.com>
2024-02-08 13:02:00 +0700

    :+1:

--- Assignment3/2.c
+++ Assignment3/2.c
@@ -1,20 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int cnt = 0;
+typedef struct node{
+    int value;
+    struct node* next;
+}node;
+
+node* head = NULL;
+node* tail = NULL;
+
+void push(int val){
+    node* new = malloc(sizeof(node));
+    new->value = val;
+    new->next = NULL;
+    if(head == NULL){
+        head = new;
+        tail = new;
+    }else{
+        tail->next = new;
+        tail = new;
+    }
+}
 
 int main(void){
     int n,k;
     scanf("%d %d",&n,&k);
-    int v[n];
     for(int i=0;i<n;i++){
-        scanf("%d",&v[i]);
+        int tmp;
+        scanf("%d",&tmp);
+        push(tmp);
     }
-    int ans = 0;
-    int i = 1;
-    while(i <= n){
-        ans = (ans+k) % i;
-        i++;
+    tail->next = head;
+    int cnt = 1;
+    int remain = n;
+    node* ptr = head;
+    node* prev = tail;
+    node* next;
+    while(remain > 1){
+        next = ptr->next;
+        if(cnt < k){
+            cnt++;
+        }else{
+            cnt = 1;
+            prev->next = next;
+            remain--;
+            free(ptr);
+        }
+        prev = ptr;
+        ptr = next;
     }
-    printf("%d",v[ans]);
-    return 0;
+    printf("%d",ptr->value);
 }
